8b75103cf89a6c720202d08e4b6acd8e44a7d560
Erik Hernandez <erikestuardo_161@hotmail.com>
2020-09-01 08:49:15 -0600

    add DOM EVENTS

--- app.js
+++ app.js
@@ -22,9 +22,16 @@ class UI {
 
 }
 
-
 // DOM EVENTS
 document.getElementById('product-form')
-.addEventListener('submit', function(){
-document.getElementById('name').value
-})
+.addEventListener('submit', function () {
+    const name= document.getElementById('name').value;
+    const price= document.getElementById('price').value;
+    const year= document.getElementById('year').value;
+
+    console.log(name,price,year);
+
+    //evento que deja de refrescar la pagina a la hora de enviar datos por submit
+    //cada vez que se envian datos desde un formulario la pagina se refresca
+    // para poder recibir luego eventos que un servidor envia, la respuesta que un servidor envia!
+});
